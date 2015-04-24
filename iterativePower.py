1888dd0fb30f0a894f0ae57a3fa96789259985a1
Tim Griffith <grifmang@gmail.com>
2015-04-23 23:51:23 -0400

    Delete iterativePower.py

--- iterativePower.py
+++
@@ -1,8 +0,0 @@
-def iterativePower(x,p):
-	result = 1
-	for turn in range(p):
-		result = turn ** x
-    		print('iteration:' + str(turn + 1)+ ' current result:' + str(result))
-	return result
-
-iterativePower(2,4)
